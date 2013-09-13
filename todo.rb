6a46053f29236702b4ba16e784bb5a72a2f94546
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 15:49:26 -0700

    started controller

--- todo.rb
+++ todo.rb
@@ -1,3 +1,17 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+sentence = ARGV
+command = sentence.shift
+
+Task.complete(task_id)
+
+case command
+when "list" then puts Task.all
+when "add" then Task.create!(sentence.join(" "))
+when "delete" then Task.destroy(task_id)
+when "complete" then Task.complete(task_id)
+else raise ArgumentError "Please select list, add, delete, or complete"
+end
+
