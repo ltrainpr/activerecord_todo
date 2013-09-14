a60f71e44ac8e02d57ac82c5134526ef826d1302
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 17:27:02 -0700

    schema and model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
+require_relative '../../config/application'
+
 class Task < ActiveRecord::Base
+
+  def complete!
+    self.complete = true
+    save
+  end
+
+  def to_s
+      "#{id}. [#{complete ? 'x' : ' '}] #{description}"
+  end
 end
