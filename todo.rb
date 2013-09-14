c8ed979e1f323ed85f782db344b47ad2740971e8
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 18:30:42 -0700

    fixed merge inconsistencies

--- todo.rb
+++ todo.rb
@@ -1,5 +1,6 @@
 require_relative 'config/application'
 
+
 # puts "Put your application code in #{File.expand_path(__FILE__)}"
 
 sentence = ARGV
@@ -7,9 +8,9 @@ command = sentence.shift
 
 case command
 when "list" then puts Task.all
-when "add" then Task.create!(sentence.join(" "))
-when "delete" then Task.destroy(task_id)
-when "complete" then Task.find(sentence).complete!
+when "add" then Task.create!(description: sentence.join(" "))
+when "delete" then Task.destroy(sentence.first)
+when "complete" then Task.find(sentence.first).complete!
 else raise ArgumentError, "Please select list, add, delete, or complete"
 end
 
