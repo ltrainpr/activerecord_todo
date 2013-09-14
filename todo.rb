42fc735650110682ae47f3359fc5b72b97138ba3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 17:57:27 -0700

    switched to complete!

--- todo.rb
+++ todo.rb
@@ -9,7 +9,7 @@ case command
 when "list" then puts Task.all
 when "add" then Task.create!(sentence.join(" "))
 when "delete" then Task.destroy(task_id)
-when "complete" then Task.complete(task_id)
+when "complete" then Task.find(sentence).complete!
 else raise ArgumentError, "Please select list, add, delete, or complete"
 end
 
